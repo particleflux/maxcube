68e34976947c44b698ea206c74f5698373eef55a
Stefan Linke <particleflux@gmail.com>
2017-01-12 00:06:55 +0100

    Add L Message

---
+++ messages/MessageL.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace particleflux\MaxCube\messages;
+
+
+use particleflux\MaxCube\models\Device;
+
+class MessageL extends Message
+{
+    const MODE_BIT_MASK = 0x03;
+
+
+    public function parse()
+    {
+        $devices = [];
+        $line = base64_decode($this->rawMessage);
+        $messageLength = ord($line[0]);
+
+        $device = new Device();
+        $device->rfAddress = unpack('H*', substr($line, 1, 3));
+        $device->mode = ord($line[6]) & self::MODE_BIT_MASK;
+
+        $devices[] = $device;
+
+        return $devices;
+    }
+}
---
+++ tests/parser/MessageLTest.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: stefan
+ * Date: 1/11/17
+ * Time: 10:48 PM
+ */
+
+namespace parser;
+
+
+use particleflux\MaxCube\messages\MessageL;
+use particleflux\MaxCube\models\Device;
+use particleflux\MaxCube\tests\TestCase;
+
+
+class MessageLTest extends TestCase
+{
+    /**
+     * @dataProvider parseProvider
+     *
+     * @param string $rawMessage
+     * @param array $decodedAttributes
+     */
+    public function testParse($rawMessage, $decodedAttributes)
+    {
+        $message = new MessageL($rawMessage);
+        $devices = $message->parse();
+        $this->assertInternalType('array', $devices);
+        $this->assertEquals(count($decodedAttributes), count($devices));
+//        $this->assertInstanceOf(Device::class, $devices);
+
+//        foreach ($decodedAttributes as $name => $value) {
+//            $this->assertObjectHasAttribute($name, $device, "expected attribute $name missing");
+//            $this->assertEquals($value, $device->$name, "Attribute $name has wrong value '{$device->$name}', expected '$value'");
+//        }
+    }
+
+    public function parseProvider()
+    {
+        return [
+            [
+                'CxUK4gkSGWQ9AK8A',
+                [
+                    [
+                        'rfAddress' => '09e4f5',
+                        'mode'      => Device::MODE_MANUAL,
+                    ],
+                ],
+            ],
+
+        ];
+    }
+}
